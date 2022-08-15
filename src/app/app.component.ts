656cf1eda81b0f6cd5f4813c0410cf6366113ab1
Debasish Nandi <debasish.nandi397@gmail.com>
2022-08-15 23:20:46 +0530

    working on input section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SettingDialogComponent } from './components/setting-dialog/setting-dialog.component';
@@ -12,15 +12,36 @@ import { AppConstant } from './utils/app-constant';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'JCalc';
 
   // form controls
-  metalFC = new FormControl('g916');
+  inputFG = new FormGroup({
+    metalFC: new FormControl('g916'),
+    mcTypeFC: new FormControl('pgm'),
+    fixedMc: new FormControl(''),
+    itemWeightFC: new FormControl(''),
+  });
+
 
   constructor(private dialog: MatDialog,
     private storageService: StorageService,
     private snackBar: MatSnackBar) { }
+
+  ngOnInit() {
+    // by default 'per gram' M.C is selected. So Fixed M.C field is disabled
+    this.inputFG.controls['fixedMc'].disable();
+  }
+
+  onMCTypeChange(event: any) {
+    console.log(event.value);
+    let mcType = event.value;
+    if (mcType === 'pgm') {
+      this.inputFG.controls['fixedMc'].disable();
+    } else if (mcType === 'ppc') {
+      this.inputFG.controls['fixedMc'].enable();
+    }
+  }
 
   openSettingDialog() {
     const dialogRef = this.dialog.open(SettingDialogComponent, { disableClose: true });
